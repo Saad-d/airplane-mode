27c5c387d15a4a65b88be6731d377b8fc5368cc8
Saad-d <saadaltafkhan456@gmail.com>
2024-09-29 08:53:56 +0530

    Change the  color of the report in revenue_by_ailine.py green to Green And remove extra space btwn full name in flight_passenger.py

--- airplane_mode/airplane_mode/report/revenue_by_airline/revenue_by_airline.py
+++ airplane_mode/airplane_mode/report/revenue_by_airline/revenue_by_airline.py
@@ -32,6 +32,6 @@ def execute(filters=None):
 	summery = [{
 		"label": "Total Revenue",
         "value": total_revenue,
-        "indicator": "green"
+        "indicator": "Green"
 	}]
 	return columns, data, 'Here is the report', chart, summery
